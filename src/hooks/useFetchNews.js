9005bbb597a6ef3054eda16d1517473c2c57684d
Ahmad <ahmed.ibrahim.bp2019@gmail.com>
2024-07-15 10:50:43 +0300

    updated

--- src/hooks/useFetchNews.js
+++ src/hooks/useFetchNews.js
@@ -2,6 +2,16 @@
 import { useState, useEffect } from 'react';
 import { getNews } from '../api/newsAPI';
 
+/**
+ * A custom React hook that fetches news articles from an API.
+ *
+ * @param {number} [page=1] - The page number of the news articles to fetch.
+ * @returns {object} - An object containing the fetched news articles, the total number of results, any error that occurred, and the loading state.
+ * @property {array} articles - The array of news articles fetched from the API.
+ * @property {number} totalResults - The total number of news articles available.
+ * @property {string|null} error - The error message if an error occurred during the fetch.
+ * @property {boolean} loading - A boolean indicating whether the data is currently being fetched.
+ */
 export const useFetchNews = (page = 1) => {
   const [articles, setArticles] = useState([]);
   const [totalResults, setTotalResults] = useState(0);
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -12,6 +12,11 @@ export const useForm = ({ initialValues, onSubmit }) => {
     }));
   };
 
+  /**
+   * Handles the form submission by preventing the default form behavior and calling the provided `onSubmit` function with the current form data.
+   *
+   * @param {React.FormEvent<HTMLFormElement>} e - The form submission event.
+   */
   const handleSubmit = (e) => {
     e.preventDefault();
     onSubmit(formData);
