2d6ac76faaedb7bfcc39ab59b021a4a8645d3be5
Ahmad <156530723+MasterWithAhmad@users.noreply.github.com>
2024-07-25 13:20:25 +0300

    Update newsAPI.js
    Updated

--- src/api/newsAPI.js
+++ src/api/newsAPI.js
@@ -1,7 +1,7 @@
 // src/api/newsAPI.js
 import axios from 'axios';
 
-const API_KEY = 'd90b98bb81fc4e328e9911d1465f9ceb'; // Replace with your NewsAPI key
+const API_KEY = 'YOUR API KEY HERE'; // Replace with your NewsAPI key
 const BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us';
 
 export const getNews = async (page = 1, pageSize = 6) => {
